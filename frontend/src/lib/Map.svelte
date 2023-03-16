05f42bd4df2d19a699b479ed628195461277e7b9
Ethan <me@ethan.link>
2023-03-16 21:44:06 +1100

    Add maplibre-gl map to frontend

---
+++ frontend/src/lib/Map.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { onMount, onDestroy } from "svelte";
+  import { Map } from "maplibre-gl";
+  import "maplibre-gl/dist/maplibre-gl.css";
+
+  let map;
+  let mapContainer;
+
+  onMount(() => {
+    const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
+
+    map = new Map({
+      container: mapContainer,
+      style: window.location.origin + "/style.json", // stylesheet location
+      center: [151.2, -33.8], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  });
+
+  onDestroy(() => {
+    map.remove();
+  });
+</script>
+
+<div class="map" id="map" bind:this={mapContainer} />
+
+<style>
+  .map {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+  }
+</style>
