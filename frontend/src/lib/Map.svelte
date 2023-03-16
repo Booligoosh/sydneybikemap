b9e6fb92bfedc8452812f4e2f34999decd216441
Ethan <me@ethan.link>
2023-03-17 09:10:56 +1100

    Update frontend map bounds

--- frontend/src/lib/Map.svelte
+++ frontend/src/lib/Map.svelte
@@ -19,7 +19,7 @@
       style: window.location.origin + "/style.json", // stylesheet location
       center: [151.2, -33.8], // starting position [lng, lat]
       zoom: 9, // starting zoom
-      maxBounds: [150.83, -34.05999990000001, 151.33, -33.679999900000009], // copied from metadata.json
+      maxBounds: [149.526157, -34.946458, 152.571173, -32.026925], // copied from bbox sent to osmconvert command in update-tiles.yml
       customAttribution:
         "&copy; SydneyBikeMap &copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors",
       hash: true,
