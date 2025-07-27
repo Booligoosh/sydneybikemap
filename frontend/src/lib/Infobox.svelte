7c4b5147ed4441065169c8f442cba1b6365dfbd8
Ethan <me@ethan.link>
2025-07-27 19:58:31 +1000

    Use @carto-au/svelte Panel

--- frontend/src/lib/Infobox.svelte
+++ frontend/src/lib/Infobox.svelte
@@ -1,63 +1,31 @@
 <script lang="ts">
   import Legend from "$lib/Legend.svelte";
   import Logo from "$lib/Logo.svelte";
-  import { Icon } from "@steeze-ui/svelte-icon";
-  import { ChevronDown } from "@steeze-ui/heroicons";
-  import { cn } from "$lib/utils";
+  import { Panel } from "@carto-au/svelte";
 
   // Only start expanded if they've never visited the site before (or not since the latest update)
-  let expanded = localStorage.getItem("lastVisitedVersion") !== "3";
+  const expanded = localStorage.getItem("lastVisitedVersion") !== "3";
 </script>
 
-<div
-  class={cn(
-    "inset-5 z-10 mx-auto h-fit w-full overflow-hidden border-b bg-white sm:fixed sm:w-auto sm:max-w-sm sm:rounded-lg sm:border lg:mx-0",
-    expanded ? "border-gray-500" : "border-gray-300",
-  )}
->
-  <section>
-    <button
-      class="group/header grid w-full cursor-pointer grid-cols-[1fr_auto] p-3 text-left"
-      on:click={() => {
-        // Toggle expanded state
-        expanded = !expanded;
-        // Trigger map to resize (as on sm and below, its area will change when the infobox is expanded/collapsed)
-        setTimeout(() => window.dispatchEvent(new Event("resize")), 0);
-      }}
-    >
+<Panel hideableName="legend" showHideableInitially={expanded}>
+  {#snippet staticHeader()}
+    <div class="p-3">
       <Logo />
-      <div
-        class="flex aspect-square h-full items-center justify-center justify-self-end rounded-full border border-gray-200 text-gray-600 transition-transform group-hover/header:bg-gray-50 group-active/header:bg-gray-100"
-        style:transform={expanded ? "rotate(180deg)" : ""}
-      >
-        <Icon src={ChevronDown} theme="micro" class="w-5" />
-      </div>
-    </button>
-  </section>
-  <!--
-    Based on this trick https://stackoverflow.com/a/8331169
-    Don't transition at sm and below as it leads to janky map resizing
-  -->
-  <div
-    class="overflow-hidden sm:transition-[max-height]"
-    style:max-height={expanded ? "100vh" : 0}
-  >
-    <div
-      class={cn(
-        "flex flex-col gap-3 border-t p-3",
-        expanded ? "border-gray-500" : "border-gray-300",
-      )}
-    >
-      <!-- <section><p>Your guide to Sydney&rsquo;s cycle network 🚲️</p></section> -->
-      <!-- <section>
-        <input
-          placeholder="Search&hellip;"
-          class="rounded-md border border-gray-400 py-2 px-3 w-full text-md hover:bg-gray-50 focus:bg-gray-100 focus:outline-none"
-        />
-      </section> -->
+    </div>
+  {/snippet}
+  <!-- {#snippet staticFooter()}
+    <div class="p-3 sm:pt-0">
+      <input
+        placeholder="Search&hellip;"
+        class="w-full rounded-lg border border-gray-300 bg-gray-50 px-3 py-2 text-sm hover:bg-gray-50 focus:bg-gray-100 focus:outline-none"
+      />
+    </div>
+  {/snippet} -->
+  {#snippet hideable()}
+    <div class="flex flex-col border-t border-gray-500 p-3">
       <Legend />
     </div>
-    <footer class="border-t border-gray-500 bg-[#a1e3b8] p-3 pt-2">
+    <footer class="border-t border-inherit bg-[#a1e3b8] p-3 pt-2">
       <div class="mt-1 text-xs">
         Built by <a href="https://ethan.link" target="_blank">Ethan</a> | Data
         &copy;
@@ -84,5 +52,5 @@
         >
       </div>
     </footer>
-  </div>
-</div>
+  {/snippet}
+</Panel>
