9a5bfa2f56e46eb66ee9a7e62bbf08a3bc550f4c
Ethan <me@ethan.link>
2025-07-27 19:43:10 +1000

    Fix expanded state

--- frontend/src/hooks.client.ts
+++ frontend/src/hooks.client.ts
@@ -3,16 +3,6 @@ import type { ClientInit } from "@sveltejs/kit";
 import { registerSW } from "virtual:pwa-register";
 
 export const init: ClientInit = () => {
-  // Future proof with a version so we can perform 'migrations' or show explainers
-  // about a change in visualisation to users who were used to something else.
-  //
-  // For now, we're using the presence of this (don't care about the number) to see
-  // if the user has visited the site before, so if we should start the infobox as
-  // expanded or collapsed.
-  //
-  // This will always get set *after* the initial render (due to being in onMount).
-  localStorage.setItem("lastVisitedVersion", "3");
-
   // When page loads, if there's no hash and we have a hash saved, use that
   if (!window.location.hash && localStorage.getItem("lastHash")) {
     window.location.hash = localStorage.getItem("lastHash") ?? "";
