8300eccec944ac8deedd0c636e868c8938abc0f1
Ethan <me@ethan.link>
2025-07-27 19:58:31 +1000

    Use @carto-au/svelte Panel

--- frontend/src/lib/Legend.svelte
+++ frontend/src/lib/Legend.svelte
@@ -5,41 +5,36 @@
   let infoModalType: "separatedCycleway" | "sharedPath" | null = null;
 </script>
 
-<section>
-  <h2 class="mb-1 font-bold">Legend</h2>
-  <div class="mb-1 grid grid-cols-[auto_1fr] items-center gap-2">
-    <LegendItem
-      text="Separated cycleway"
-      color="hsl(141, 54%, 28%)"
-      lineStyle="solid"
-      showInfoBtn={true}
-      on:infoclick={() => (infoModalType = "separatedCycleway")}
-    />
-    <LegendItem
-      text="Shared path"
-      color="hsl(211, 61%, 56%)"
-      lineStyle="solid"
-      showInfoBtn={true}
-      on:infoclick={() => (infoModalType = "sharedPath")}
-    />
-    <LegendItem
-      text="Safe speed street (≤30km/h limit)"
-      color="hsl(31, 29%, 58%)"
-      lineStyle="solid"
-    />
-    <LegendItem
-      text="Cycleway under construction"
-      color="hsl(6, 93%, 71%)"
-      lineStyle="dashed"
-    />
-    <LegendItem
-      text="Planned cycleway"
-      color="hsl(300, 50%, 70%)"
-      lineStyle="dashed"
-    />
-  </div>
-  <LegendInfoModal
-    type={infoModalType}
-    on:close={() => (infoModalType = null)}
+<div class="grid grid-cols-[auto_1fr] items-center gap-2">
+  <LegendItem
+    text="Separated cycleway"
+    color="hsl(141, 54%, 28%)"
+    lineStyle="solid"
+    showInfoBtn={true}
+    on:infoclick={() => (infoModalType = "separatedCycleway")}
   />
-</section>
+  <LegendItem
+    text="Shared path"
+    color="hsl(211, 61%, 56%)"
+    lineStyle="solid"
+    showInfoBtn={true}
+    on:infoclick={() => (infoModalType = "sharedPath")}
+  />
+  <LegendItem
+    text="Safe speed street (≤30km/h limit)"
+    color="hsl(31, 29%, 58%)"
+    lineStyle="solid"
+  />
+  <LegendItem
+    text="Cycleway under construction"
+    color="hsl(6, 93%, 71%)"
+    lineStyle="dashed"
+  />
+  <LegendItem
+    text="Planned cycleway"
+    color="hsl(300, 50%, 70%)"
+    lineStyle="dashed"
+  />
+</div>
+
+<LegendInfoModal type={infoModalType} on:close={() => (infoModalType = null)} />
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,7 +2,7 @@
 import defaultTheme from "tailwindcss/defaultTheme";
 
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,svelte}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,svelte}", "node_modules/@carto-au/svelte/**/*.{js,ts,jsx,tsx,svelte}"],
   theme: {
     extend: {
       fontFamily: {
